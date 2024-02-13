0dd37f89c1c08a6a7a00bb700e964906c5189fa8
Clerton <clerton.sampaio@druid.com.br>
2024-02-13 19:55:18 -0300

    Update database.py to use environment variable for database URL

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQL_ALCHEMY_DATABASE_URL = "postgresql://postgres_user:s3cr3t@localhost/inventory_db"
+SQL_ALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", "postgresql://postgres_user:s3cr3t@db/inventory_db"
+)
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
