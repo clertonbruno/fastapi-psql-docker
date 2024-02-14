a6d80b054ab7ea674a4cf00b9282915f0329a238
Clerton <clerton.sampaio@druid.com.br>
2024-02-14 03:47:41 -0300

    Refactor database.py to use os_environ for DATABASE_URL

--- database.py
+++ database.py
@@ -1,12 +1,10 @@
-import os
+from os import environ as os_environ
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQL_ALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL", "postgresql://postgres:s3cr3t@db/inventory_db"
-)
+SQL_ALCHEMY_DATABASE_URL = os_environ["DATABASE_URL"]
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 
@@ -16,6 +14,13 @@ Base = declarative_base()
 
 
 def get_db():
+    """
+    Returns a database session.
+
+    Yields:
+        SessionLocal: The database session.
+
+    """
     db = SessionLocal()
     try:
         yield db
